6018835cab1ad2192aafaaa353ae50aa90b81237
hirop4 <hirop4@gmail.com>
2014-07-31 09:49:48 +0900

    Impl layouts

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,26 +1,169 @@
-var socket = io.connect('http://localhost:3000');
-socket.on('connect', function(msg) {
-  console.log("connect");
+$(function() {
+  var FADE_TIME = 150;
+  var TYPING_TIMER_LENGTH = 400;
+  var COLORS = [ '#e21400', '#91580f', '#f8a700', '#f78b00' ];
+  var $window = $(window);
+  var $usernameInput = $('.usernameInput');
+  var $messages = $('.messages');
+  var $inputMessage = $('.inputMessage');
+
+  var $loginPage = $('.login.page');
+  var $chatPage = $('.chat.page');
+
+  var username;
+  var connected = false;
+  var typing = false;
+  var lastTypingTime;
+  var $currentInput = $usernameInput.focus();
+
+//  var socket = io.connect('http://localhost:3000');
+  var socket = io();
+
+  function addParticipantsMessage (data) {
+    var message = '';
+    if (data.numUsers === 1) {
+      message += "there's 1 participant";
+    } else {
+      message += "there're" + data.numUsers + " participants";
+    }
+    log(message);
+  }
+
+  function setUsername () {
+    username = cleanInput($usernameInput.val().trim());
+    if (username) {
+      $loginPage.fadeOut();
+      $chatPage.show();
+      $loginPage.off('click');
+      $currentInput = $inputMessage.focus();
+
+      socket.emit('add user', username);
+    }
+  }
+
+  function sendMessage () {
+    var message = $inputMessage.val();
+    message = cleanInput(message);
+    if (message && connected) {
+      $inputMessage.val('');
+      addChatMessage({
+        username: username,
+        message: message
+      });
+      socket.emit('new message', message);
+    }
+  }
+
+  function log (message, options) {
+    var $el = $('<li>').addClass('log').text(message);
+    addMessageElement($el, options);
+  }
+
+  function addChatMessage (data, options) {
+    var $typingMessages = getTypingMessages(data);
+    options = options || {};
+    if ($typingMessages.length !== 0) {
+      options.fade = false;
+      $typingMessages.remove();
+    }
+
+    var $usernameDiv = $('<span class="username"/>').text(data.username).css('color', getUsernameColor(data.username));
+    var $messageBodyDiv = $('<span class="messageBody">').text(data.message);
+    var typingClass = data.typing ? 'typing' : '';
+    var $messageDiv = $('<li class="message"/>').data('username', data.username).addClass(typingClass).append($usernameDiv, $messageBodyDiv);
+    addMessageElement($messageDiv, options);
+  }
+
+  function addMessageElement (el, options) {
+    var $el = $(el);
+    if (!options) {
+      options = {};
+    }
+    if (typeof options.fade === 'undefined') {
+      options.fade = true;
+    }
+    if (typeof options.prepend === 'undefined') {
+      options.prepend = false;
+    }
+
+    if (options.fade) {
+      $el.hide().fadeIn(FADE_TIME);
+    }
+    if (options.prepend) {
+      $messages.prepend($el);
+    }
+    else {
+      $messages.append($el);
+    }
+    $messages[0].scrollTop = $messages[0].scrollHeight;
+  }
+
+  function cleanInput (input) {
+    return $('<div\>').text(input).text();
+  }
+
+  function updateTyping () {
+    if (connected) {
+
+    }
+  }
+
+  function getTypingMessages (data) {
+    return $('.typing.message').filter(function (i) {
+      return $(this).data('username') === data.username;
+    });
+  }
+
+  function getUsernameColor (username) {
+    var hash = 7;
+    for (var i=0; i < username.length; i++) {
+      hash = username.charCodeAt(i) + (hash << 5) - hash;
+    }
+    var index = Math.abs(hash % COLORS.length);
+    return COLORS[index];
+  }
+
+  $window.keydown(function (event) {
+    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+      $currentInput.focus();
+    }
+    if (event.which === 13) {
+      if (username) {
+        sendMessage();
+        socket.emit('stop typing');
+        typing = false;
+      } else {
+        setUsername();
+      }
+    }
+  });
+
+  $inputMessage.on('input', function () {
+    updateTyping();
+  });
+
+  $loginPage.click(function () {
+    $currentInput.focus();
+  });
+
+  $inputMessage.click(function () {
+    $inputMessage.focus();
+  });
+
+  socket.on('login', function (data) {
+    connected = true;
+    var message = "Welcome to Socket.IO Chat &mdash; ";
+    log(message, { prepend: true });
+    addParticipantsMessage(data);
+  });
+
+  socket.on('new message', function (data) {
+    addChatMessage(data);
+  });
+
+  socket.on('user joined', function (data) {
+    log(data.username + ' joined');
+    addParticipantsMessage(data);
+  });
+  
 });
-
-// メッセージを受けたとき
-socket.on('message', function(msg) {
-  // メッセージを画面に表示する
-  document.getElementById("receiveMsg").innerHTML = msg.value;
-});
-
-// メッセージを送る
-function SendMsg() {
-  var msg = document.getElementById("message").value;
-  // メッセージを発射する
-  socket.emit('message', { value: msg });
-}
-
-// 切断する 
-function DisConnect() {
-  // メッセージを発射する
-  socket.emit('message', { value: '切断しました' });
-  // socketを切断する
-  socket.disconnect();
-}
-
